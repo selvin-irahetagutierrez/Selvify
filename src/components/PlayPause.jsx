3addaab1d5692f3fca1b5a41042c0eeb36254905
Selvinjr99 <103534433+Selvinjr99@users.noreply.github.com>
2023-02-16 17:27:28 -0500

    Finished Home Page

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,6 +1,16 @@
-import{FaPauseCircle,FaPlayCircles} from "react-icons/fa"
-const PlayPause = ({isPlaying,activeSong,song,handlePause,handlePlay }) => (
-  <div>Loader</div>
-);
+import{FaPauseCircle,FaPlayCircle} from "react-icons/fa"
+const PlayPause = ({isPlaying,activeSong,song,handlePause,handlePlay }) => (isPlaying
+   && activeSong ?.title===song.title?
+  (<FaPauseCircle
+    size={35}
+    className="text-gray-300"
+    onClick={handlePause}
+  />):
+  (<FaPlayCircle
+    size={35}
+    className="text-gray-300"
+    onClick={handlePlay}
+  />)
+  );
 
 export default PlayPause;
